f387a86dc8daa1ed37e4ce2e6fa0112f182f9db4
Sara Borg <sara.borg.2001@gmail.com>
2022-05-04 15:36:15 +0200

    created selection menu

--- AgilePacman/Assets/Scripts/StartGame.cs
+++ AgilePacman/Assets/Scripts/StartGame.cs
@@ -2,21 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class NewGame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class StartGame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    // Start is called before the first frame update
     public GameObject button;
-    bool gameIsStarted;
-    public GameObject canvas;
     public GameObject selectionCanvas;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        gameIsStarted = false;
-        canvas.SetActive(true);
-        selectionCanvas.SetActive(false);
-
+        selectionCanvas.SetActive(true);
         Time.timeScale = 0;
     }
 
@@ -28,10 +23,9 @@ public class NewGame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void click()
     {
-        canvas.SetActive(false);
-        selectionCanvas.SetActive(true);
-        // Debug.Log("init new game..");
-        // Time.timeScale = 1;
+        selectionCanvas.SetActive(false);
+        // Debug.Log("init start game..");
+        Time.timeScale = 1;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -44,3 +38,4 @@ public class NewGame : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         button.GetComponentInChildren<Text>().fontSize = 28;
     }
 }
+
