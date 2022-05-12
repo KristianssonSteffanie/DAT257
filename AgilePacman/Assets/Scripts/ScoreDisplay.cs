e5f7bd40142a78bf6d6ed5bbfef3b066a984babb
blixtcorp <jakobpersson007@gmail.com>
2022-05-12 13:53:16 +0200

    highscore display

--- AgilePacman/Assets/Scripts/ScoreDisplay.cs
+++ AgilePacman/Assets/Scripts/ScoreDisplay.cs
@@ -7,13 +7,22 @@ public class ScoreDisplay : MonoBehaviour
 {
     private GameManager gameManager;
     private int score;
+
+    private int highScore;
+    private string key = "HighScore";
+
     public TextMeshProUGUI scoreDisp;
+    public TextMeshProUGUI highScoreDisp;
+
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
         score = 0;
+
+        highScore = PlayerPrefs.GetInt(key, 0);
+
     }
 
     // Update is called once per frame
@@ -22,5 +31,18 @@ public class ScoreDisplay : MonoBehaviour
         score = gameManager.GetScore();
 
         scoreDisp.text = score.ToString();
+
+        highScoreDisp.text = highScore.ToString();
+    }
+
+    void OnDisable()
+    {
+
+        //If our scoree is greter than highscore, set new higscore and save.
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt(key, score);
+            PlayerPrefs.Save();
+        }
     }
 }
